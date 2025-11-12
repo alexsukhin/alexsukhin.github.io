dc74d5d2ec12a1d269ae67bdf791280924e49bb6
Alexander Sukhin <alex@sukhin.net>
2025-11-12 23:59:31 +0000

    Updates

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-12 text-left text-gray-500 text-sm">
-      &copy; 2025 Alexander Sukhin — London, United Kingdom
-    </footer>
-  );
-}
