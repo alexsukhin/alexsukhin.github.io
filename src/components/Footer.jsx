cbe53b95f16276b1c5a11e82897e12ab25cc50a4
Alexander Sukhin <alex@sukhin.net>
2025-11-12 23:48:45 +0000

    Initial commit: created personal website page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-12 text-left text-gray-500 text-sm">
+      &copy; 2025 Alexander Sukhin — London, United Kingdom
+    </footer>
+  );
+}
